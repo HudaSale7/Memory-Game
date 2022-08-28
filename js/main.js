76ddf5ebd26adaea939ef99b73f3a346fdee82ab
HudaSale7 <hudasaleh640@gmail.com>
2022-08-28 02:48:08 +0200

    done

--- js/main.js
+++ js/main.js
@@ -1,5 +1,28 @@
 //////////////////////////////// Start //////////////////////////////////////////
 
+//overlay variables
+let overLayStart = `    <div class="overlay-start">
+<div class="content">
+    <div class="input">
+        <input type="text" autofocus placeholder="Enter your name">
+    </div>
+    <div class="btn">Start</div>
+</div>
+</div>`;
+
+let overLayEnd = `    <div class="overlay-end">
+<div class="content">
+    <h3 class="winner-loser">YOU LOST THE GAME ^^</h3>
+    <div class="option">
+        <div class="btn">Again</div>
+        <div class="cancel">Cancel</div>
+    </div>
+</div>
+</div>`;
+
+document.body.innerHTML = overLayEnd + document.body.innerHTML;
+document.body.innerHTML = overLayStart + document.body.innerHTML;
+
 //global variable
 let userInput = document.querySelector(".input input");
 let startBtn = document.querySelectorAll(".btn");
@@ -15,8 +38,6 @@ let audio = new Audio(
   "audio/zapsplat_multimedia_game_tone_bright_plucked_positive_tone_001_88490.mp3"
 );
 
-//////////////////////////////////////////////////////////
-
 
 // add event to bottom start and play again
 startBtn.forEach((e) => {
@@ -25,14 +46,17 @@ startBtn.forEach((e) => {
       userName.innerHTML = userInput.value;
     }
 
+      
     //in case he want to play again the wrong tries back to zero
     wrong.innerHTML = "0";
     wrong.style.color = "black";
 
+    
       
     //hide the start and end layer
     layer.style.display = "none";
-    endLayer.style.display = "none";
+      endLayer.style.display = "none";
+      
 
     //make the element arranged in random every time he finish
     for (let i = 0; i < 20; i++) {
@@ -40,10 +64,12 @@ startBtn.forEach((e) => {
         Math.floor(Math.random() * boxes.length),
         Math.floor(Math.random() * boxes.length)
       );
-    }
+      }
+      
 
     //rotate to the back for a little bit
     rotate();
+      
 
     //back to play
     setTimeout(rotateBack, 2000);
@@ -51,15 +77,18 @@ startBtn.forEach((e) => {
       
     //adding click event on every card
     boxes.forEach((element) => {
-      element.addEventListener("click", (e) => {
+        element.addEventListener("click", (e) => {
+          
         //to count the number of clicked card
         let count = 0;
+            
         //array to carry the index of clicked card
         let arrayOfIndex = [];
 
         //on click flip the card and add attribute to it
         e.currentTarget.style.transform = "rotateY(180deg)";
         e.currentTarget.setAttribute("state", "click");
+            
 
         //check how many card has an attribute state
         boxes.forEach((element, index) => {
@@ -68,6 +97,8 @@ startBtn.forEach((e) => {
             arrayOfIndex.push(index);
           }
         });
+
+        
         if (count === 2) {
           //prevent click event on any element
           unClick();
@@ -81,7 +112,8 @@ startBtn.forEach((e) => {
   });
 });
 
-//////////////////////////////////////////////////////////
+
+
 
 //swap function
 function swapIndex(indexOne, indexTwo) {
@@ -90,7 +122,7 @@ function swapIndex(indexOne, indexTwo) {
   boxes[indexTwo].innerHTML = temp;
 }
 
-//////////////////////////////////////////////////////////
+
 
 //rotate to back img
 function rotate() {
@@ -99,7 +131,7 @@ function rotate() {
   });
 }
 
-//////////////////////////////////////////////////////////
+
 
 //rotate to front img
 function rotateBack() {
@@ -108,7 +140,7 @@ function rotateBack() {
   });
 }
 
-//////////////////////////////////////////////////////////
+
 
 
 //when he click on two card make the rest of them unclickable
@@ -118,7 +150,7 @@ function unClick() {
   });
 }
 
-//////////////////////////////////////////////////////////
+
 
 
 //return to clickable state again after finishing the check
@@ -130,13 +162,14 @@ function click() {
   });
 }
 
-//////////////////////////////////////////////////////////
+
 
 
 // if he choose right or wrong
 function checkSimilarity(index) {
   //check on them by its index
-  if (boxes[index[0]].innerHTML !== boxes[index[1]].innerHTML) {
+    if (boxes[index[0]].innerHTML !== boxes[index[1]].innerHTML) {
+      
     //on case choosing wrong
     setTimeout(function () {
       boxes[index[0]].style.transform = "rotateY(0deg)";
@@ -157,7 +190,9 @@ function checkSimilarity(index) {
     //if he choose correct
     audio.play();
     endGame++;
-    if (endGame === 10) {
+        
+        if (endGame === 10) {
+        
       //when he finish the game
       endLayer.style.display = "flex";
       if (wrong.innerHTML > 10) {
@@ -179,5 +214,7 @@ function checkSimilarity(index) {
 cancel.onclick = function () {
   endLayer.style.display = "none";
 };
+
+
 
 //////////////////////////////// End //////////////////////////////////////////
